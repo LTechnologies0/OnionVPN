255f6941f4685b0ed2fd5c11b4e417fae0da3e24
LTechnologies0 <LTechnologies0@users.noreply.github.com>
2026-07-28 16:49:46 +0200

    chore(release): bump versionName/versionCode to 0.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ltechnologies.onionphone.onionvpn"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 7
-        versionName = "0.3.0"
+        versionCode = 8
+        versionName = "0.3.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
